65e9b67936283b480f4f5a6e6bcac348c65e7e96
Boofny <Brandonpizano7557@gmail.com>
2026-08-27 13:46:11 -0400

    basic health check for git

--- lua/teleport/health.lua
+++ lua/teleport/health.lua
@@ -1,2 +1,13 @@
-local Health = {}
-return Health
+local M = {}
+
+M.check = function ()
+  vim.health.start("Teleport report")
+
+  if vim.fn.executable("git") == 1 then
+    vim.health.ok("git binary found.")
+  else
+    vim.health.error("git binary not found, install in order to use this plugin")
+  end
+end
+
+return M
